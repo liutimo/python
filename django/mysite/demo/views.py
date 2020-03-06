a3a7c057718afd3eac4e257ecc170da459c43efe
liutimo <779564531@qq.com>
2020-03-06 19:56:16 +0800

    1. {% url '' %} 使用该标签可以方便的修改URL

--- django/mysite/demo/views.py
+++ django/mysite/demo/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
-# Create your views here.
 
+# Create your views here.
+@login_required(login_url='login')
 def index(request):
-    return render(request, 'register.html', {})
+    return render(request, 'login.html')
 
 
 def register(request):
@@ -19,21 +21,21 @@ def register(request):
         user.is_active = 0
         user.save()
         return render(request, 'login.html')
-    return render(request, 'register.html', {'error_msg' : '注册失败'})
+    return render(request, 'register.html', {'error_msg': '注册失败'})
 
 
 def login(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        print('%s : %s' % (username , password))
-        user = authenticate(username=username, password=password)
+        print('%s : %s' % (username, password))
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             if user.is_active:
                 return HttpResponse("用户已激活")
             else:
                 return HttpResponse("用户未激活")
         else:
-            return render(request, 'login.html', {'error_msg' : '用户名密码错误'})
+            return render(request, 'login.html', {'error_msg': '用户名密码错误'})
     else:
         return render(request, 'login.html', {'error_msg': '用户名密码错误'})
